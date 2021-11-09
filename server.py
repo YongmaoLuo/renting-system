e746b0787bfd1ebfe72a8a9a3144fd4e64f6e84a
markwu372 <zwu372@gatech.edu>
2021-11-09 15:23:02 -0500

    version 1

--- server.py
+++ server.py
@@ -94,7 +94,7 @@ def teardown_request(exception):
 # see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 #
-@app.route('/')
+@app.route('/index')
 def index():
   """
   request is a special object that Flask provides to access web request information:
@@ -193,12 +193,44 @@ def top_max_chart():
 
     return render_template("top_max_chart.html", zipcode=zip_codes, price=prices)
 
+@app.route('/agency')
+def agency():
+  """
+  request is a special object that Flask provides to access web request information:
+
+  request.method:   "GET" or "POST"
+  request.form:     if the browser submitted a form, this contains the data in the form
+  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+
+  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+  """
+
+  # DEBUG: this is debugging code to see what request looks like
+  # print(request.args)
+
+
+  #
+  # example of a database query
+  #
+  cursor = g.conn.execute("SELECT name FROM test")
+  names = []
+  for result in cursor:
+    names.append(result['name'])  # can also be accessed using result[0]
+  cursor.close()
+
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
   name = request.form['name']
   g.conn.execute('INSERT INTO test VALUES (@0)', name)
+  return redirect('/index')
+
+
+@app.route('/addContract', methods=['POST'])
+def addContract():
+  name = request.form['name']
+  g.conn.execute('INSERT INTO contract VALUES (@0)', name)
   return redirect('/')
 
 
