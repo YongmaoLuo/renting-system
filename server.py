a17b5c5a753443bfa602c7d27efdbcd2d318aefa
YongmaoLuo <576146819@qq.com>
2021-11-09 21:07:54 -0500

    [mode changed] set default parameters for running

--- server.py
+++ server.py
@@ -385,7 +385,7 @@ if __name__ == "__main__":
 
         HOST, PORT = host, port
         print("running on %s:%d" % (HOST, PORT))
-        app.run(host=HOST, port=PORT, debug=true, threaded=threaded)
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
     run()
